ee747090c594c9235d0b1e78b08c45a98322bef5
John Wells <molekewl2017@gmail.com>
2019-10-31 02:00:56 -0700

    mvp

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // code away!
 const server = require('./server');
 
-server.listen(4000, () => console.log('Server listening on port 4000'))
+server.listen(4000, () => console.log('Server listening on port 4000'));
--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,54 @@
 const express = 'express';
 
+const Posts = require('./postDb');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+  Posts.get()
+  .then(posts => res.status(200).json(posts))
+  .catch(err => res.status(500).json({ error: 'Failed to retrieve posts' }))
 });
 
 router.get('/:id', (req, res) => {
+  const id = req.params.id;
 
+  Posts.getById(id)
+  .then(post => {
+    post
+    ? res.status(200).json(post)
+    : res.status(404).json({ message: `Could not find post with ID of ${id}` })
+  })
+  .catch(err => res.status(500).json({ error: `Failed to retrieve post with ID of ${id}` }))
 });
 
 router.delete('/:id', (req, res) => {
-
+  const id = req.params.id
+  
+  Posts.remove(id)
+  .then(deleted => {
+    deleted > 0
+    ? res.status(200).json(deleted)
+    : res.status(404).json({ message: `Could not find post with ID of ${id}` })
+  })
+  .catch(err => res.status(500).json({ error: `Failed to remove post with ID of ${id}` }))
 });
 
 router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const updated = req.body;
 
+  if (!updated.text) res.status(400.json({ message: 'Provide text please'}));
+
+  Posts.update(id, updated)
+  .then(updated => {
+    if (updated) {
+      Posts.getById(id)
+      .then(post => res.status(200).json(post))
+    }
+    else res.status(404).json({ message: `Could not find post with ID of ${id}` })
+  })
+  .catch(err => res.status(500).json({ error: `Failed to retrieve post with ID of ${id}` }))
 });
 
 // custom middleware
